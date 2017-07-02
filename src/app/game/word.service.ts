0352b4b19b1b01468f4b2446801a836484d5e359
aksrugiw <aksrugiw@interia.pl>
2017-07-02 16:09:57 +0200

    Fix api url

--- src/app/game/word.service.ts
+++ src/app/game/word.service.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class WordService {
-    private apiUrl = 'http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=11&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
+    private apiUrl = 'https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=11&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
 
     constructor(private _http: Http) { }
     
